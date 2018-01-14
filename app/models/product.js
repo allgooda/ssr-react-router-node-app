c31dd6fdeab3bf31c29a02a26182fda9678f4863
Adam Allgood <allgoodadam@gmail.com>
2018-01-13 16:31:23 -0800

    created product model and connected to mongod

--- app/models/product.js
+++ app/models/product.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const productSchema = new mongoose.Schema({
+var productSchema = new mongoose.Schema({
   name: String,
   type: String,
   price: { type: Number, default: 0 }
--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,15 @@
 import app from './app';
+import mongoose from 'mongoose'
 
 const port = process.env.PORT || 8080;
+const dbUri = 'mongodb://localhost/furniture';
+mongoose.connect(dbUri);
+
+mongoose.connection
+  .once('open', () => console.log('DB CONNECTED'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
 
 app.listen(port);
 console.log(`Listening at http://localhost:${port}`);
